f283082b4cbe455bcfb5e919eeadd45983f0c93b
sandysan77 <148611431+sandysan77@users.noreply.github.com>
2024-01-06 13:43:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var h1=document.createElement("h1");
+document.body.append(h1);
+
+var i=10;
+setTimeout(()=>{
+    document.querySelector('h1').innerText = i;
+setTimeout(()=>{
+   document.querySelector('h1').innerText = i=i-1;
+    setTimeout(()=>{
+        document.querySelector('h1').innerText = i=i-1;
+        setTimeout(()=>{
+            document.querySelector('h1').innerText = i=i-1;
+            setTimeout(()=>{
+                document.querySelector('h1').innerText =  i=i-1;
+                setTimeout(()=>{
+                    document.querySelector('h1').innerText = i=i-1;
+                    setTimeout(()=>{
+                        document.querySelector('h1').innerText = i=i-1;
+                        setTimeout(()=>{
+                            document.querySelector('h1').innerText = i=i-1;
+                            setTimeout(()=>{
+                                document.querySelector('h1').innerText = i=i-1;
+                                setTimeout(()=>{
+                                    document.querySelector('h1').innerText = i=i-1;
+                                    setTimeout(()=>{
+                                        document.querySelector('h1').innerText =  i=i-1;
+                                        setTimeout(()=>{
+                                            document.querySelector('h1').innerText = "Happy Independence Day"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
